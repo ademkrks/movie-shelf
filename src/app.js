51665a6a3513b4cd537aeb615fb9c5889503f7b8
ademkrks <aliademkarakass@gmail.com>
2026-08-04 21:20:46 +0300

    Configure environment variables with dotenv

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 const express = require('express');
+
 const movieRoutes = require('./routes/movie.routes');
+
 const logger =require('./middleware/logger');
+
 const errorHandler =require('./middleware/errorHandler');
+
 //express uygulamasını oluşturur
 const app =express();
+
 //Json verilerini okuyabilmek için 
 app.use(express.json());
+
 //gelen istekleri loglamak için
 app.use(logger);
+
 //film route'ları
 app.use("/movies", movieRoutes);
+
 //Global hata yakalayıcı
 app.use(errorHandler);
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
+//Ortam değişkenlerini yükler
+require("dotenv").config();
 //app.js dosyasındaki express uygulamasını içe aktarır
 const app = require('./app');
 //sunucunun çalışacağı port
-const PORT =3000;
+const PORT =process.env.PORT || 3000;
     //sunucuyu başlatır
     app.listen(PORT,()=>{
         console.log(`Server http://localhost:${PORT} adresinde çalışıyor`);
