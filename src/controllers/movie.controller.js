aa91ae9ace114ee677ae599f03b2ba53b25fbd63
ademkrks <aliademkarakass@gmail.com>
2026-08-04 11:31:42 +0300

    Add movie service

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -1,9 +1,8 @@
-const getMovies = (req,res)=> {
-    res.json({
-        success:true,
-        message:"Filmler Başarıyla Getirildi"
-    });
-};
+const movieService =require("../services/movie.service");
+const getMovies=(req,res)=>{
+    const result = movieService.getMovies();
+    res.json(result);
+}
 module.exports ={
     getMovies,
 };
---
+++ src/services/movie.service.js
@@ -0,0 +1,9 @@
+const getMovies=()=>{
+    return{
+        success:true,
+        message: "Filmler Service Katmanından Geldi"
+    };
+};
+module.exports={
+    getMovies,
+};
