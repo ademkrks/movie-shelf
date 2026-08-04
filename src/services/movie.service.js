db8f7d47bf67d0ceb1c0c11bb0a8655e1a98c229
ademkrks <aliademkarakass@gmail.com>
2026-08-04 11:44:45 +0300

    Return movie list from service

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -1,8 +1,21 @@
 const getMovies=()=>{
-    return{
-        success:true,
-        message: "Filmler Service Katmanından Geldi"
-    };
+    return [
+        {
+            id:1,
+            title:"Inception",
+            year:2010
+        },
+        {
+            id:2,
+            title:"Interstellar",
+            year:2014
+        },
+        {
+            id:3,
+            title:"The Dark Knight",
+            year:2008
+        }
+    ];
 };
 module.exports={
     getMovies,
