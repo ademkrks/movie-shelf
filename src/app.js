747308c8ede9ae24adbf060cefa6865576936722
ademkrks <aliademkarakass@gmail.com>
2026-08-04 17:27:38 +0300

    Add request logger middleware

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const movieRoutes = require('./routes/movie.routes');
+const logger =require('./middleware/logger');
 
 const app =express();
 
 app.use(express.json());
+app.use(logger);
+
 app.use("/movies", movieRoutes);
 
 module.exports = app;
